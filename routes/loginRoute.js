2972054c1646bd0c74417c4b624015ae63e05669
harshsoni-harsh <52278293+harshsoni-harsh@users.noreply.github.com>
2024-04-09 14:50:32 +0530

    configured /docs with swagger

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,7 +1,81 @@
+/**
+ * @swagger
+ * components:
+ *     securitySchemes:
+ *         bearerAuth:
+ *             type: http
+ *             scheme: bearer
+ *             bearerFormat: JWT
+ *     responses:
+ *         UnauthorizedError:
+ *             description: JWT token is missing or invalid
+ *     schemas:
+ *         User:
+ *             type: object
+ *             required:
+ *                 - name
+ *                 - email
+ *                 - password
+ *             properties:
+ *                 _id:
+ *                     type: string
+ *                     description: Auto generated id of User
+ *                 name:
+ *                     type: string
+ *                     description: Name of the user
+ *                 role:
+ *                     type: string
+ *                     description: Role of the user
+ *                 email:
+ *                     type: string
+ *                     description: E-mail of the user
+ *                 password:
+ *                     type: string
+ *                     description: Password of the user
+ */
+/**
+ * @swagger
+ * tags:
+ *     name: User-api
+ *     description: User management API
+ * /login:
+ *     post:
+ *         summary: Logs in the user to get JWT token
+ *         tags: [User-api]
+ *         requestBody:
+ *             description: Login Credentials
+ *             required: true
+ *             content:
+ *                 application/json:
+ *                     schema:
+ *                         type: object
+ *                         properties:
+ *                             email:
+ *                                 type: string
+ *                             password:
+ *                                 type: string
+ *                     example:
+ *                         email: "jane@xyz.com"
+ *                         password: "default"
+ *         responses:
+ *             200:
+ *                 description: The JWT Token
+ *                 content:
+ *                     application/json:
+ *                         schema:
+ *                             type: object
+ *                             properties:
+ *                                 jwtToken:
+ *                                     type: string
+ *                                     description: JWT Token, that'll be used to perform management operations
+ *             401:
+ *                 description: Authentication Error - Invalid Credentials
+ */
+
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const User = require('../models/user')
+const User = require("../models/user");
 
 router.post("/login", async (req, res) => {
   let { email, password } = req.body;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,142 @@
+/**
+ * @swagger
+ * paths:
+ *   /users:
+ *     get:
+ *       summary: Fetches details of all users
+ *       security:
+ *         - bearerAuth: []
+ *       tags: [User-api]
+ *       responses:
+ *         200:
+ *           description: All users details
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   $ref: '#/components/schemas/User'
+ *         401:
+ *           $ref: '#/components/responses/UnauthorizedError'
+ *   /user:
+ *     post:
+ *       summary: Adds a new user
+ *       security:
+ *         - bearerAuth: []
+ *       tags: [User-api]
+ *       requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 userDetails:
+ *                   type: object
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                       description: Name of the user
+ *                     role:
+ *                       type: string
+ *                       description: Role of the user
+ *                     email:
+ *                       type: string
+ *                       description: E-mail of the user
+ *                     password:
+ *                       type: string
+ *                       description: Password of the user
+ *       responses:
+ *         201:
+ *           description: User created successfully
+ *         409:
+ *           description: User already exists with the given mail-id.
+ *         400:
+ *           description: User details are missing
+ */
+/**
+ * @swagger
+ * paths:
+ *   /user/{id}:
+ *     get:
+ *       summary: Fetches the user details with the given id
+ *       security:
+ *         - bearerAuth: []
+ *       tags: [User-api]
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: User ID
+ *           required: true
+ *           schema:
+ *             type: string
+ *       responses:
+ *         200:
+ *           description: User details
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/User'
+ *         404:
+ *           description: User not found
+ *     patch:
+ *       summary: Updates the user details with the given id
+ *       security:
+ *         - bearerAuth: []
+ *       tags: [User-api]
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: User ID
+ *           required: true
+ *           schema:
+ *             type: string
+ *       requestBody:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 userDetails:
+ *                   type: object
+ *                   properties:
+ *                     name:
+ *                       type: string
+ *                       description: Name of the user
+ *                     role:
+ *                       type: string
+ *                       description: Role of the user
+ *                     email:
+ *                       type: string
+ *                       description: E-mail id of the user
+ *                     password:
+ *                       type: string
+ *                       description: Password of the user
+ *       responses:
+ *         200:
+ *           description: User details updated successfully
+ *         400:
+ *           description: User details were not entered
+ *         404:
+ *           description: User not found
+ *     delete:
+ *       summary: Deletes the user with the given id
+ *       security:
+ *         - bearerAuth: []
+ *       tags: [User-api]
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: User ID
+ *           required: true
+ *           schema:
+ *             type: string
+ *       responses:
+ *         204:
+ *           description: User deleted successfully
+ *         404:
+ *           description: User not found
+ */
+
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
@@ -18,9 +157,13 @@ router.post("/user", authenticateToken, async (req, res) => {
         let rep = await User.insertMany([{ name, role, email, password }]);
         console.log(rep);
       } else {
-        res.status(409).send("A user already exists with the given mail-id. Please use a different mail-id");
+        res
+          .status(409)
+          .send(
+            "A user already exists with the given mail-id. Please use a different mail-id"
+          );
       }
-      res.status(200).send("User added successfully");
+      res.status(201).send("User added successfully");
     } else {
       res
         .status(400)
@@ -35,8 +178,12 @@ router.post("/user", authenticateToken, async (req, res) => {
 
 router.get("/user/:id", authenticateToken, async (req, res) => {
   const { id } = req.params;
-  const userDetails = await User.find({ _id: id });
-  res.send(userDetails);
+  try {
+    const userDetails = await User.find({ _id: id });
+    res.send(userDetails[0]);
+  } catch {
+    res.status(404).send("User not found");
+  }
 });
 
 router.patch("/user/:id", authenticateToken, async (req, res) => {
@@ -62,7 +209,7 @@ router.delete("/user/:id", authenticateToken, async (req, res) => {
   const { id } = req.params;
   const { deletedCount } = await User.deleteOne({ _id: id });
   if (deletedCount) {
-    res.send("User deleted successfully");
+    res.status(204).send("User deleted successfully");
   } else {
     res.status(404).send("User not found");
   }
